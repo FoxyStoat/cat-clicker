af61d73a7e977f653ebc38f41fb44cf1fb001d5c
FoxyStoat <carol.hodgson@hotmail.com>
2018-06-08 10:40:49 +0100

    feat: Add event listener for cat clicks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-alert('I am connected');
+const luckyCat = document.querySelector(".lucky-cat");
+let counter = 0;
+
+/* 
+EventListener: On click of image increment counter and
+update counter html ".cat-clicks"
+*/
+luckyCat.addEventListener('click', function(){
+	const catClicks = document.querySelector(".cat-clicks");
+  counter ++;
+  catClicks.innerHTML = "Number Of Clicks: " + counter;
+});
